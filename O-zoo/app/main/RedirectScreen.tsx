a289478a496465b78d745f458bb114f4adf4c15b
shsim23 <shsim23@kaist.ac.kr>
2025-07-19 17:56:05 +0900

    login good

--- O-zoo/app/main/RedirectScreen.tsx
+++ O-zoo/app/main/RedirectScreen.tsx
@@ -7,9 +7,10 @@ const RedirectScreen = () => {
   const params = useLocalSearchParams();
 
   useEffect(() => {
-    if (params.login === "success") {
+    const {login, token} = params;
+    if (login === "success" && typeof token === "string") {
       // 로그인 성공이면 main 화면으로 이동
-      router.replace("/main");
+      router.replace({pathname: "/main", params: {token},});
     } else {
       // 실패 등 처리 (예: 로그인 페이지로 이동)
       router.replace("/login");
